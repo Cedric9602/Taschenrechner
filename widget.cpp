dbc807655ed8dd6cdebba0c24bf9ce8b3d956c35
teleaffe <koalamassl@web.de>
2018-04-16 15:26:38 +0200

    ztuj

--- widget.cpp
+++ widget.cpp
@@ -1,11 +1,25 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include<QPushButton>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    QPushButton *testbutton1 = new QPushButton(this);
+    testbutton1->setGeometry(20,20,100,20);
+    testbutton1->setText("Knopf1");
+
+    QPushButton *testbutton2 = new QPushButton(this);
+    testbutton2->setGeometry(120,20,100,20);
+    testbutton2->setText("Knopf2");
+
+    QPushButton *testbutton3 = new QPushButton(this);
+    testbutton3->setGeometry(220,20,100,20);
+    testbutton3->setText("Knopf3");
+
 }
 
 Widget::~Widget()
