729f1739715412cead4acd614313bf365db11791
teleaffe <koalamassl@web.de>
2018-04-25 11:28:38 +0200

    rechenfkt2.0

--- widget.cpp
+++ widget.cpp
@@ -306,3 +306,368 @@ void Widget::DEL_clicked(){
 void Widget::AC_clicked(){
     Eingabetext->setText("");
 }
+
+
+/*// Basic.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+
+#include "stdafx.h"
+#include<iostream>
+#include<fstream>
+#include <math.h>
+#include <memory>
+#include <string>
+#include <vector>
+#include <forward_list>
+
+using namespace std;
+
+struct Zeichen {
+    string Art;
+    double Zahl;
+    char Operand;
+};
+
+vector<Zeichen> Aufteilen(string S);
+double Rechnen(vector<Zeichen> V);
+bool Klammern(vector<Zeichen> V);
+bool Wurzel(vector<Zeichen> V);
+
+double Rechenfunktion(string S);
+
+
+int main()
+{
+
+    //string test = "((((sqrt(5*5)))))";
+    //cout << test << endl;
+
+
+
+    //cout << Rechenfunktion("sqrt(25)") << endl;
+    //(2 + 3)*(5 + (4 * sqrt(4 * 4))) + 100
+    auto tmp = Rechenfunktion("5^(1+1*3)");
+
+    system("pause");
+    return 0;
+}
+
+double Rechenfunktion(string S) {
+    return(Rechnen(Aufteilen(S)));
+}
+
+
+
+
+vector<Zeichen> Aufteilen(string str) {
+
+    string s = "";
+    for (int64_t i = 0; i < str.size(); i++) {
+
+        if ((str.at(i) == 's') && (str.at(i + 1) == 'q') && (str.at(i + 2) == 'r') && (str.at(i + 3) == 't') && (str.at(i + 4) == '(')) {
+
+            s.push_back('s');
+            s.push_back('(');
+            i += 4;
+
+        }
+        else {
+            s.push_back(str.at(i));
+        }
+
+    }
+
+    s = '(' + s + ')';
+
+
+    //Temp Variablen
+    bool letzteZahl = false;
+    string TmpZahl = "";
+    double TmpDouble = 0;
+    Zeichen TmpZeichen;
+
+    string Operatoren = "+-/^(*)s";
+
+
+    vector <Zeichen> Zerteilt;
+
+    //Durch String iterieren
+
+    for (int64_t i = 0; i < s.size(); i++) {
+
+        //Zahlen erkennen
+        if (((s.at(i) >= '0') && (s.at(i) <= '9')) || (s.at(i) == '.')) {
+            letzteZahl = true;
+
+            if (letzteZahl == true) {
+                TmpZahl.push_back(s.at(i));
+            }
+            else {
+                TmpZahl = s.at(i);
+            }
+
+        }
+        //keine Zahl mehr
+        else if ((letzteZahl == true)) {
+            letzteZahl = false;
+            TmpDouble = stod(TmpZahl, NULL);
+
+            TmpZeichen.Art = "Zahl";
+            TmpZeichen.Operand = '#';
+            TmpZeichen.Zahl = TmpDouble;
+            Zerteilt.push_back(TmpZeichen);
+
+            TmpZahl = "";
+            TmpDouble = 0;
+            TmpZeichen.Art = "";
+            TmpZeichen.Zahl = 0;
+            TmpZeichen.Operand = '#';
+        }
+
+        //Letztes Element eine Zahl
+        if ((((s.at(i) >= '0') && (s.at(i) <= '9')) || (s.at(i) == '.')) && (i == s.size() - 1)) {
+            letzteZahl = false;
+            TmpDouble = stod(TmpZahl, NULL);
+
+            TmpZeichen.Art = "Zahl";
+            TmpZeichen.Operand = '#';
+            TmpZeichen.Zahl = TmpDouble;
+            Zerteilt.push_back(TmpZeichen);
+
+            TmpZahl = "";
+            TmpDouble = 0;
+            TmpZeichen.Art = "";
+            TmpZeichen.Zahl = 0;
+            TmpZeichen.Operand = '#';
+
+
+        }
+
+
+
+        //Operator erkennen
+        if ((Operatoren.find(s.at(i))) != string::npos) {
+            TmpZeichen.Art = "Operator";
+            TmpZeichen.Operand = s.at(i);
+            TmpZeichen.Zahl = 0;
+
+            Zerteilt.push_back(TmpZeichen);
+
+            TmpZahl = "";
+            TmpDouble = 0;
+            TmpZeichen.Art = "";
+            TmpZeichen.Zahl = 0;
+            TmpZeichen.Operand = '#';
+
+        }
+    }
+
+    return Zerteilt;
+}
+
+
+
+
+double Rechnen(vector<Zeichen> V) {
+    vector<Zeichen> vect = V;
+    vector<Zeichen> helpvect;
+
+    Zeichen TmpZeichen;
+    double ZwischenErg = 0;
+    double Erg;
+
+    bool eins;
+    int16_t KVorne;
+    int16_t KHinten;
+    bool Rechnung = false;
+
+
+    while (vect.size() >1) {
+
+
+        //Wurzel berechnen
+        Rechnung = false;
+        for (auto i = 0; i < vect.size()-1; i++) {
+
+            if ((vect.at(i).Operand == 's') && (vect.at(i + 1).Art == "Zahl")) {
+                Rechnung = true;
+                TmpZeichen.Art = "Zahl";
+                TmpZeichen.Operand = '#';
+                TmpZeichen.Zahl = sqrt(vect.at(i + 1).Zahl);
+                helpvect.push_back(TmpZeichen);
+
+                TmpZeichen.Art = "";
+                TmpZeichen.Zahl = 0;
+                TmpZeichen.Operand = '#';
+
+                i++;
+
+            }
+            else {
+                helpvect.push_back(vect.at(i));
+            }
+
+        }
+
+        helpvect.push_back(vect.back());
+        if (Rechnung) {
+            vect = helpvect;
+            Rechnung = false;
+        }
+        helpvect.clear();
+
+
+
+
+        //Potenz berechnen
+        for (auto i = 0; i < vect.size()-1; i++) {
+            if ((vect.at(i + 1).Operand == '^')) {
+                if ((vect.at(i).Art == "Zahl") && (vect.at(i + 1).Operand == '^') && (vect.at(i + 2).Art == "Zahl")) {
+                    Rechnung = true;
+                    TmpZeichen.Art = "Zahl";
+                    TmpZeichen.Operand = '#';
+                    TmpZeichen.Zahl = pow(vect.at(i).Zahl, vect.at(i + 2).Zahl);
+                    helpvect.push_back(TmpZeichen);
+
+                    ZwischenErg = 0;
+                    TmpZeichen.Art = "";
+                    TmpZeichen.Zahl = 0;
+                    TmpZeichen.Operand = '#';
+
+                    i += 2;
+
+                }
+                else {
+                    helpvect.push_back(vect.at(i));
+                }
+            }
+            else {
+                helpvect.push_back(vect.at(i));
+            }
+        }
+        helpvect.push_back(vect.back());
+        if (Rechnung) {
+            vect = helpvect;
+            Rechnung = false;
+        }
+        helpvect.clear();
+
+
+
+
+
+        //klammern suchen
+        eins = true;
+        for (auto hinten = 0; (hinten < vect.size()); hinten++) {
+            if (vect.at(hinten).Operand == ')') {
+
+                for (auto vorne = hinten; eins == true; vorne--) {
+
+                    if (vect.at(vorne).Operand == '(') {
+                        eins = false;
+                        KVorne = vorne;
+                        KHinten = hinten;
+                        break;
+
+                    }
+                }
+            }
+
+            if (eins == false) {
+                break;
+            }
+        }
+
+
+
+
+        //Vor Klammer
+        for (auto i = 0; i < KVorne; i++) {
+            helpvect.push_back(vect.at(i));
+        }
+
+
+        //Einzelne zahl in klammer
+        if ((KHinten - KVorne) == 2) {
+
+            ZwischenErg = vect.at(KVorne + 1).Zahl;
+            TmpZeichen.Art = "Zahl";
+            TmpZeichen.Operand = '#';
+            TmpZeichen.Zahl = ZwischenErg;
+            helpvect.push_back(TmpZeichen);
+
+            ZwischenErg = 0;
+            TmpZeichen.Art = "";
+            TmpZeichen.Zahl = 0;
+            TmpZeichen.Operand = '#';
+
+        }
+
+        if ((KHinten - KVorne) > 2) {
+            ZwischenErg = vect.at(KVorne + 1).Zahl;
+
+            for (auto i = KVorne+1; i < KHinten-2; i++) {
+
+                switch (vect.at(i+1).Operand) {
+
+                case '+': ZwischenErg = ZwischenErg + vect.at(i + 2).Zahl; break;
+                case '-': ZwischenErg = ZwischenErg - vect.at(i + 2).Zahl; break;
+                case '*': ZwischenErg = ZwischenErg * vect.at(i + 2).Zahl; break;
+                case '/': ZwischenErg = ZwischenErg / vect.at(i + 2).Zahl; break;
+                }
+
+            }
+
+
+            TmpZeichen.Art = "Zahl";
+            TmpZeichen.Operand = '#';
+            TmpZeichen.Zahl = ZwischenErg;
+            helpvect.push_back(TmpZeichen);
+
+            ZwischenErg = 0;
+            TmpZeichen.Art = "";
+            TmpZeichen.Zahl = 0;
+            TmpZeichen.Operand = '#';
+
+
+        }
+
+
+        //nach Klammer
+
+        for (auto i = (KHinten + 1); i < vect.size(); i++) {
+            helpvect.push_back(vect.at(i));
+        }
+
+        vect = helpvect;
+        helpvect.clear();
+
+
+    }
+
+    return vect.at(0).Zahl;
+}
+
+
+bool Klammern(vector<Zeichen> V) {
+    bool out = false;
+    for (auto i = 0; i < V.size(); i++) {
+        if (V.at(i).Operand == ')') {
+            out = true;
+            break;
+        }
+    }
+    return out;
+}
+
+
+bool Wurzel(vector<Zeichen> V) {
+    bool out = false;
+    for (auto i = 0; i < V.size(); i++) {
+        if (V.at(i).Operand == 's') {
+            out = true;
+            break;
+        }
+    }
+    return out;
+}
+*/
